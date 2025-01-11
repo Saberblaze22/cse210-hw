2d11883b65bf692a1c1447f2897bc81ddb07776d
Saberblaze22 <Comiccooldylan@gmail.com>
2025-01-11 13:37:07 -0700

    commit

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name?");
+        string first=console.readline()
+        console.WriteLine("what is your last name?");
+        string last=console.readline()
+        console.WriteLine($"your name is {last}, {first} {last}")
     }
-}
